426e09391ea269d8b810b0e3eb927e0568fce51a
Наталья Комарова <natkom54@yandex.ru>
2023-08-12 16:23:34 +0700

    седьмое домашнее задание с исправлением

--- homework7/task7.2.py
+++ homework7/task7.2.py
@@ -12,10 +12,11 @@
 # 6 12 18 24 30 36
 
 
+
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)] # Нумерация строк и столбцов идет с единицы
+    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
     for i in a:
-        print(*[f"{x:>3}" for x in i])
+        print(*[f"{x}" for x in i])
 
 
 print_operation_table(lambda x, y: x * y)
